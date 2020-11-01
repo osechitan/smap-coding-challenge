a7d2a8e820af467a8a7f2fe01c81cebfc022b660
osechitan <toshine212@hotmail.co.jp>
2020-11-01 20:21:36 +0900

    add user and consumption models

--- dashboard/consumption/models.py
+++ dashboard/consumption/models.py
@@ -3,4 +3,29 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    """User Model"""
+    class Meta:
+        db_table = 'user'
+
+    area = models.CharField(verbose_name='area', max_length=2)
+    tariff = models.CharField(verbose_name='tariff', max_length=2)
+
+    def __str__(self):
+        return self.id
+
+
+class Consumption(models.Model):
+    """Consuption Model"""
+    class Meta:
+        db_table = 'consumption'
+
+    user = models.ForeignKey(User,
+                             verbose_name='user',
+                             on_delete=models.CASCADE)
+    datetime = models.DateTimeField(verbose_name='datetime')
+    consumption = models.FloatField(verbose_name='consumption')
+
+    def __str__(self):
+        return self.id
--- dashboard/dashboard/settings.py
+++ dashboard/dashboard/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '=stpmw2pq0*w(_2t4t90#j_*b$%#7q0e3jk(we#ttyp#y(#$qe'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'consumption',
+    'consumption.apps.ConsumptionConfig',
 ]
 
 MIDDLEWARE = [
