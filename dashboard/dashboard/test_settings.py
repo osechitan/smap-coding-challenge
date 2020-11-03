fa3833db418a509c8e79cbefb36a52c5b3aeae59
osechitan <toshine212@hotmail.co.jp>
2020-11-03 09:39:18 +0900

    delete test_settings.py

--- dashboard/dashboard/test_settings.py
+++
@@ -1,8 +0,0 @@
-import pathlib
-from dashboard.settings import * # noqa
-
-
-USER_FILE_DEFAULT = str(pathlib.Path('__file__')) + \
-                    '/consumption/tests/data/user_data.csv'
-CONSUMPTION_FILE_DEFAULT = str(pathlib.Path('__file__')) + \
-                           '/consumption/tests/data/consumption'
